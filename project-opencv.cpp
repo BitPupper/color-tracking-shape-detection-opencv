e465c09533bf2ed6afc8df1de86fcf478600aeff
Nono <38902034+BitPupper@users.noreply.github.com>
2021-02-26 04:06:35 +0900

    Update project-opencv.cpp

--- project-opencv.cpp
+++ project-opencv.cpp
@@ -8,9 +8,6 @@ using namespace std;
 const char* wndname = "Shape Detection";
 int thresh = 50, N = 11;
 
-// helper function:
-// finds a cosine of angle between vectors
-// from pt0->pt1 and from pt0->pt2
 static double angle(Point pt1, Point pt2, Point pt0)
 {
     double dx1 = pt1.x - pt0.x;
@@ -20,7 +17,6 @@ static double angle(Point pt1, Point pt2, Point pt0)
     return (dx1 * dx2 + dy1 * dy2) / sqrt((dx1 * dx1 + dy1 * dy1) * (dx2 * dx2 + dy2 * dy2) + 1e-10);
 }
 
-// returns sequence of squares detected on the image.
 static void findSquares(const Mat& image, vector<vector<Point> >& squares, vector<vector<Point> >& triangles)
 {
 
@@ -29,56 +25,35 @@ static void findSquares(const Mat& image, vector<vector<Point> >& squares, vecto
 
     Mat pyr, timg, gray0(image.size(), CV_8U), gray;
 
-    // down-scale and upscale the image to filter out the noise
     pyrDown(image, pyr, Size(image.cols / 2, image.rows / 2));
     pyrUp(pyr, timg, image.size());
     vector<vector<Point> > contours;
 
-    // find squares in every color plane of the image
     for (int c = 0; c < 3; c++)
     {
         int ch[] = { c, 0 };
         mixChannels(&timg, 1, &gray0, 1, ch, 1);
 
-        // try several threshold levels
         for (int l = 0; l < N; l++)
         {
-            // hack: use Canny instead of zero threshold level.
-            // Canny helps to catch squares with gradient shading
             if (l == 0)
             {
-                // apply Canny. Take the upper threshold from slider
-                // and set the lower to 0 (which forces edges merging)
                 Canny(gray0, gray, 0, thresh, 5);
-                // dilate canny output to remove potential
-                // holes between edge segments
                 dilate(gray, gray, Mat(), Point(-1, -1));
             }
             else
             {
-                // apply threshold if l!=0:
-                //     tgray(x,y) = gray(x,y) < (l+1)*255/N ? 255 : 0
                 gray = gray0 >= (l + 1) * 255 / N;
             }
-
-            // find contours and store them all as a list
+            
             findContours(gray, contours, RETR_LIST, CHAIN_APPROX_SIMPLE);
 
             vector<Point> approx;
 
-            // test each contour
             for (size_t i = 0; i < contours.size(); i++)
             {
-                // approximate contour with accuracy proportional
-                // to the contour perimeter
                 approxPolyDP(contours[i], approx, arcLength(contours[i], true) * 0.02, true);
 
-                // square contours should have 4 vertices after approximation
-                // relatively large area (to filter out noisy contours)
-                // and be convex.
-                // Note: absolute value of an area is used because
-                // area may be positive or negative - in accordance with the
-                // contour orientation
                 if (approx.size() == 4 &&
                     fabs(contourArea(approx)) > 1000 &&
                     isContourConvex(approx))
@@ -87,7 +62,6 @@ static void findSquares(const Mat& image, vector<vector<Point> >& squares, vecto
 
                     for (int j = 2; j < 5; j++)
                     {
-                        // find the maximum cosine of the angle between joint edges
                         double cosine = fabs(angle(approx[j % 4], approx[j - 2], approx[j - 1]));
                         maxCosine = MAX(maxCosine, cosine);
                     }
@@ -95,33 +69,13 @@ static void findSquares(const Mat& image, vector<vector<Point> >& squares, vecto
                     if (maxCosine < 0.3)
                         squares.push_back(approx);
 
-                    // if cosines of all angles are small
-                    // (all angles are ~90 degree) then write quandrange
-                    // vertices to resultant sequence
-
                 }
-                /*else if (approx.size() == 3 &&
-                    fabs(contourArea(approx)) > 1000 &&
-                    isContourConvex(approx))
-                {
-                    double maxCosine = 0;
 
-                    for (int j = 2; j < 5; j++)
-                    {
-                        // find the maximum cosine of the angle between joint edges
-                        double cosine = fabs(angle(approx[j % 4], approx[j - 2], approx[j - 1]));
-                        maxCosine = MAX(maxCosine, cosine);
-                    }
-
-                    if (maxCosine < 0.3)
-                        triangles.push_back(approx);
-                }*/
             }
         }
     }
 }
 
-// the function draws all the squares in the image
 static void drawSquares(Mat& image, const vector<vector<Point> >& squares, const vector<vector<Point> >& triangles)
 {
 
@@ -132,15 +86,6 @@ static void drawSquares(Mat& image, const vector<vector<Point> >& squares, const
         polylines(image, &p, &n, 1, true, Scalar(0, 255, 0), 3, LINE_AA);
         //putText(image, "square", squares[i][0], 1, 0.5, Scalar(125, 60, 20));
     }
-    /*
-    for (size_t i = 0; i < triangles.size(); i++)
-    {
-        const Point* p = &squares[i][0];
-        int n = (int)triangles[i].size();
-        polylines(image, &p, &n, 1, true, Scalar(255, 0, 0), 3, LINE_AA);
-        //putText(image, "square", squares[i][0], 1, 0.5, Scalar(125, 60, 20));
-    }*/
-
 
     //imwrite(wndname, image);
 }
@@ -166,7 +111,6 @@ int main(int argc, char** argv)
     int iLowV = 60;
     int iHighV = 255;
 
-    //Create trackbars in "Control" window
     createTrackbar("LowH", "Control", &iLowH, 179); //Hue (0 - 179)
     createTrackbar("HighH", "Control", &iHighH, 179);
 
@@ -179,7 +123,6 @@ int main(int argc, char** argv)
     int iLastX = -1;
     int iLastY = -1;
 
-    //Capture a temporary image from the camera
     Mat imgTmp;
     cap.read(imgTmp);
 
@@ -205,17 +148,14 @@ int main(int argc, char** argv)
 
         Mat imgThresholded;
 
-        inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded); //Threshold the image
-
-        //morphological opening (removes small objects from the foreground)
+        inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded); 
+        
         erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
         dilate(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
 
-        //morphological closing (removes small holes from the foreground)
         dilate(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
         erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
 
-        //Calculate the moments of the thresholded image
         Moments oMoments = moments(imgThresholded);
 
         double dM01 = oMoments.m01;
@@ -262,38 +202,3 @@ int main(int argc, char** argv)
 
     return 0;
 }
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////@
-
-//
-//int main(int argc, char** argv)
-//{
-//    static const char* names[] = { "pic1.png", "pic2.png", "pic3.png",
-//        "pic4.png", "pic5.png", "pic6.png", 0 };
-//    help(argv[0]);
-//
-//    if (argc > 1)
-//    {
-//        names[0] = argv[1];
-//        names[1] = "0";
-//    }
-//
-
-//    for (int i = 0; names[i] != 0; i++)
-//    {
-//        string filename = samples::findFile(names[i]);
-//        Mat image = imread(filename, IMREAD_COLOR);
-//        if (image.empty())
-//        {
-//            cout << "Couldn't load " << filename << endl;
-//            continue;
-//        }
-//
-
-//
-//        int c = waitKey();
-//        if (c == 27)
-//            break;
-//    }
-//
-//    return 0;
-//}
